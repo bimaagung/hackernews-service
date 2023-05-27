3c78ccd6285d625e7d2f7acd8730032f6322c38e
bimaagung <bimaagung.labs@gmail.com>
2023-05-27 19:55:48 +0700

    feat: create function GetStoryById in news firebase repository

--- internal/repository/firebase/news_firebase_repository_test.go
+++ internal/repository/firebase/news_firebase_repository_test.go
@@ -2,7 +2,10 @@ package firebaserepository_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"hackernews-service/domain"
 	mockhttpclient "hackernews-service/internal/mock/http_client"
 	firebaserepository "hackernews-service/internal/repository/firebase"
 	"io/ioutil"
@@ -58,43 +61,62 @@ func TestNewsFirebaseRepository_GetTopStories(t *testing.T) {
 
 func TestNewsFirebaseRepository_GetStoryById(t *testing.T) {
 	mockClient := &mockhttpclient.MockHTTPClient{}
+
+	expectTopStories := &domain.Story{
+		ID: 36093995,
+		By: "martincmartin",
+		Descendants: 24,
+		Kids: []int{36094267, 36094186, 36094203},
+		Score: 38,
+		Time: 1685188865,
+		Title: "Downtown San Francisco is at a tipping-point",
+		Type: "story",
+		URL: "https://www.economist.com/united-states/2023/05/25/downtown-san-francisco-is-at-a-tipping-point",
+	}
 	
-	t.Run("success get top stories", func(t *testing.T) {
+	t.Run("success getStoryById", func(t *testing.T) {
 		// Arrange
-		mockResponse := &http.Response{
-			StatusCode: http.StatusOK,
-			Body: ioutil.NopCloser(bytes.NewBufferString("[36088672, 36088783, 36087442]")),
+		buffer := new(bytes.Buffer)
+		err := json.NewEncoder(buffer).Encode(expectTopStories)
+		if err != nil {
+			t.Errorf("Error encoding story: %s", err)
+			
 		}
 
-		expectTopStories := []int{36088672, 36088783, 36087442}
+		mockResponse := &http.Response{
+			StatusCode: http.StatusOK,
+			Body: ioutil.NopCloser(bytes.NewBuffer(buffer.Bytes())),
+		}
 
-		mockClient.On("Get", "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").Return(mockResponse, nil)
+		url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json?print=pretty", expectTopStories.ID)
+		mockClient.On("Get", url).Return(mockResponse, nil)
 
 		repo := firebaserepository.NewNewsFirebaseRepository(mockClient)
 
 		// Action
-		topStories, err := repo.GetTopStories()
+		story, err := repo.GetStoryById(expectTopStories.ID)
 		if err != nil {
 			t.Errorf("GetTopStories error: %s", err)
 		}
 
 		// Assert
 		require.NoError(t, err)
-		assert.Equal(t, expectTopStories, topStories)
-		mockClient.AssertCalled(t, "Get", "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+		assert.Equal(t, expectTopStories, story)
+		mockClient.AssertCalled(t, "Get", url)
 	})
 
-	t.Run("failed can't get stories from firebase", func(t *testing.T) {
+	t.Run("failed can't getStoryById from firebase", func(t *testing.T) {
 		// Arrange
-		mockClient.On("Get", "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").Return(nil, errors.New("failed fetch firebase stories"))
-
+		url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json?print=pretty", expectTopStories.ID)
+		mockClient.On("Get", url).Return(nil, errors.New("failed fetch firebase story"))
+	
 		repo := firebaserepository.NewNewsFirebaseRepository(mockClient)
 
 		// Action
-		_, err := repo.GetTopStories()
+		_, err := repo.GetStoryById(expectTopStories.ID)
 
 		// Assert
 		require.Error(t, err)
-		mockClient.AssertCalled(t, "Get", "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+		mockClient.AssertCalled(t, "Get", url)
 	})
 }
