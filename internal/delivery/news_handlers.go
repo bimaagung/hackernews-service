fd8985649fbee18d2fb20ac333e7489e8b3c5509
bimaagung <bimaagung.labs@gmail.com>
2023-05-27 13:58:27 +0700

    core: added Continuous Integration for build and test

--- internal/delivery/news_handlers.go
+++ internal/delivery/news_handlers.go
@@ -12,15 +12,14 @@ type NewsHandler struct {
 func (h *NewsHandler) GetAll(w http.ResponseWriter, r *http.Request){
 	
 	resTopStories, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+	
 	resItemStory, err := http.Get("https://hacker-news.firebaseio.com/v0/item/36073020.json?print=pretty")
 	
-	
-
 	if err != nil {
 		return 
 	}
 
-	defer response.Body.Close()
+	defer resTopStories.Body.Close()
 
 	// Set the response headers
 	w.Header().Set("Content-Type", "application/json")
