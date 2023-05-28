674b1ebc80332b320a46c9ec38ae9d69ae9a0188
bimaagung <bimaagung.labs@gmail.com>
2023-05-28 15:28:38 +0700

    feat: added middleware cors

--- internal/delivery/router.go
+++ internal/delivery/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter(newsHandler *NewsHandler) http.Handler {
@@ -12,6 +13,16 @@ func NewRouter(newsHandler *NewsHandler) http.Handler {
 
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT","DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: true,
+		MaxAge: 300,
+	}))
+
+	router.Use(middleware.Heartbeat("/ping"))
 
 	router.Get("/news", newsHandler.GetAll)
 
